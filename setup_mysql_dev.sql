e1f27722833f90fb04cd0fad97752a7a367cc0a4
Ialbertine <a.ingabire1@alustudent.com>
2024-01-24 07:49:14 -0800

    Airbnb clone MySql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- Create a database and a user for the project
+-- Create a Database and a user for the project
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
